0a21e065354d23271237942516e3dad24a75f759
Ole Strohm <strohm99@gmail.com>
2021-12-04 00:00:17 +0000

    Quitting clients now works!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,63 @@
-#![allow(unused)]
-use std::collections::HashMap;
+use log::{error, info, trace, warn};
 use std::ffi::{c_void, CStr};
 use std::mem::MaybeUninit;
-use std::ops::Add;
 use std::ptr::{self, NonNull};
 use std::sync::atomic::{AtomicBool, Ordering};
+use x11::keysym::{XK_Tab, XK_space, XK_Q};
 use x11::xlib::{
-    BadAccess, ConfigureNotify, ConfigureRequest, CreateNotify, DestroyNotify, Display, IsViewable,
-    MapRequest, ReparentNotify, SubstructureNotifyMask, SubstructureRedirectMask, UnmapNotify,
-    Window, XCloseDisplay, XConfigureEvent, XConfigureRequestEvent, XConfigureWindow,
-    XCreateSimpleWindow, XCreateWindowEvent, XDefaultRootWindow, XDestroyWindow,
-    XDestroyWindowEvent, XDisplayName, XDisplayString, XErrorEvent, XEvent, XFree,
-    XGetWindowAttributes, XGrabServer, XMapRequestEvent, XMapWindow, XNextEvent, XOpenDisplay,
-    XQueryTree, XRemoveFromSaveSet, XReparentEvent, XReparentWindow, XSelectInput,
-    XSetErrorHandler, XSync, XUngrabServer, XUnmapEvent, XUnmapWindow, XWindowAttributes,
-    XWindowChanges,
+    BadAccess, ConfigureNotify, ConfigureRequest, CreateNotify, DestroyNotify, Display,
+    GrabModeAsync, IsViewable, KeyPress, KeyRelease, MapRequest, Mod1Mask, ReparentNotify,
+    SubstructureNotifyMask, SubstructureRedirectMask, UnmapNotify, Window, XAddToSaveSet,
+    XCloseDisplay, XConfigureEvent, XConfigureRequestEvent, XConfigureWindow, XCreateSimpleWindow,
+    XCreateWindowEvent, XDefaultRootWindow, XDestroyWindow, XDestroyWindowEvent, XDisplayName,
+    XDisplayString, XErrorEvent, XFree, XGetWindowAttributes, XGrabKey, XGrabServer,
+    XKeyPressedEvent, XKeyReleasedEvent, XKeysymToKeycode, XKillClient, XMapRequestEvent,
+    XMapWindow, XNextEvent, XOpenDisplay, XQueryTree, XRemoveFromSaveSet, XReparentEvent,
+    XReparentWindow, XSelectInput, XSetErrorHandler, XSync, XUngrabServer, XUnmapEvent,
+    XUnmapWindow, XWindowAttributes, XWindowChanges,
 };
 
+struct ClientList(Vec<(Window, Window)>);
+
+impl ClientList {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn contains(&self, w: &Window) -> bool {
+        self.0.iter().find(|(win, _)| win == w).is_some()
+    }
+
+    pub fn find(&self, w: &Window) -> Option<usize> {
+        self.0
+            .iter()
+            .enumerate()
+            .find(|(_, (win, _))| win == w)
+            .map(|(i, _)| i)
+    }
+
+    pub fn get(&self, w: &Window) -> Option<&Window> {
+        self.0.iter().find(|(win, _)| win == w).map(|(_, f)| f)
+    }
+
+    pub fn insert(&mut self, w: Window, f: Window) {
+        self.0.push((w, f));
+    }
+
+    pub fn remove(&mut self, w: &Window) {
+        if let Some(i) = self.find(w) {
+            self.0.remove(i);
+        }
+    }
+}
+
 fn main() {
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(10)
+        .init()
+        .unwrap();
+
     let wm = match WindowManager::new() {
         Some(wm) => wm,
         None => panic!("Failed to initialize window manager"),
@@ -29,7 +69,7 @@ fn main() {
 pub struct WindowManager {
     display: NonNull<Display>,
     root: Window,
-    clients: HashMap<Window, Window>,
+    clients: ClientList,
 }
 
 static WM_DETECTED: AtomicBool = AtomicBool::new(false);
@@ -39,7 +79,7 @@ impl WindowManager {
         let display = match NonNull::new(unsafe { XOpenDisplay(ptr::null()) }) {
             Some(display) => display,
             None => {
-                eprintln!("Failed to open X display: {:?}", unsafe {
+                error!("Failed to open X display: {:?}", unsafe {
                     CStr::from_ptr(XDisplayName(ptr::null()))
                 });
                 return None;
@@ -51,7 +91,7 @@ impl WindowManager {
         Some(Box::new(WindowManager {
             display,
             root,
-            clients: HashMap::new(),
+            clients: ClientList::new(),
         }))
     }
 
@@ -69,9 +109,10 @@ impl WindowManager {
             XSync(self.display.as_ptr(), 0);
 
             if WM_DETECTED.load(Ordering::Relaxed) {
-                eprintln!("Detected another window manager on display {:?}", unsafe {
+                error!(
+                    "Detected another window manager on display {:?}",
                     CStr::from_ptr(XDisplayString(self.display.as_ptr()))
-                });
+                );
                 return;
             }
 
@@ -100,6 +141,10 @@ impl WindowManager {
         assert_eq!(returned_root, self.root);
 
         unsafe {
+            info!(
+                "There were {} windows already existing",
+                num_top_level_windows
+            );
             for i in 0..num_top_level_windows {
                 self.frame(ptr::read(top_level_windows.add(i as usize)), true);
             }
@@ -108,12 +153,26 @@ impl WindowManager {
             XUngrabServer(self.display.as_ptr());
         }
 
+        unsafe {
+            XGrabKey(
+                self.display.as_ptr(),
+                XKeysymToKeycode(self.display.as_ptr(), XK_space.into()).into(),
+                Mod1Mask,
+                self.root,
+                0,
+                GrabModeAsync,
+                GrabModeAsync,
+            );
+        }
+
         loop {
             let e = unsafe {
                 let mut e = MaybeUninit::uninit();
                 XNextEvent(self.display.as_ptr(), e.as_mut_ptr());
                 e.assume_init()
             };
+
+            trace!("Event {}", e.get_type());
 
             #[allow(non_upper_case_globals)]
             match e.get_type() {
@@ -124,9 +183,30 @@ impl WindowManager {
                 CreateNotify => self.on_create_notify(XCreateWindowEvent::from(e)),
                 DestroyNotify => self.on_destroy_notify(XDestroyWindowEvent::from(e)),
                 ReparentNotify => self.on_reparent_notify(XReparentEvent::from(e)),
-                _ => eprintln!("Ignored event: {}", e.get_type()),
+                KeyPress => self.on_key_pressed(XKeyPressedEvent::from(e)),
+                KeyRelease => self.on_key_released(XKeyReleasedEvent::from(e)),
+                _ => warn!("Ignored event: {}", e.get_type()),
             }
         }
+    }
+
+    fn on_key_pressed(&mut self, e: XKeyPressedEvent) {
+        info!("key pressed: {}", e.keycode);
+        if e.keycode == unsafe { XKeysymToKeycode(self.display.as_ptr(), XK_Q.into()) }.into() {
+            // Kill client
+            info!("Killing window {}", e.window);
+            unsafe {
+                XKillClient(self.display.as_ptr(), e.window);
+            }
+        } else if e.state & Mod1Mask != 0
+            && e.keycode == unsafe { XKeysymToKeycode(self.display.as_ptr(), XK_Tab.into()) }.into()
+        {
+            self.clients.get(&e.window);
+        }
+    }
+
+    fn on_key_released(&mut self, e: XKeyReleasedEvent) {
+        info!("key released: {}", e.keycode);
     }
 
     fn frame(&mut self, w: Window, created_before_wm: bool) {
@@ -167,10 +247,23 @@ impl WindowManager {
                 frame,
                 SubstructureRedirectMask | SubstructureNotifyMask,
             );
-            XReparentWindow(self.display.as_ptr(), w, frame, 0, 0);
+            XAddToSaveSet(display, w);
+            XReparentWindow(display, w, frame, 0, 0);
             XMapWindow(display, frame);
             self.clients.insert(w, frame);
+
             // grab events
+            XGrabKey(
+                display,
+                XKeysymToKeycode(display, XK_Q.into()).into(),
+                Mod1Mask,
+                w,
+                0,
+                GrabModeAsync,
+                GrabModeAsync,
+            );
+
+            trace!("Framed window {} [{}]", w, frame);
         }
     }
 
@@ -179,6 +272,7 @@ impl WindowManager {
 
         unsafe {
             XMapWindow(self.display.as_ptr(), e.window);
+            trace!("Mapped window {}", e.window);
         }
     }
 
@@ -191,11 +285,13 @@ impl WindowManager {
             XRemoveFromSaveSet(self.display.as_ptr(), w);
             XDestroyWindow(self.display.as_ptr(), frame);
             self.clients.remove(&w);
+
+            trace!("Unframed window {} [{}]", w, frame);
         }
     }
 
     fn on_unmap_notify(&mut self, e: XUnmapEvent) {
-        if e.event != self.root && self.clients.contains_key(&e.window) {
+        if e.event != self.root && self.clients.contains(&e.window) {
             self.unframe(e.window);
         }
     }
@@ -229,24 +325,32 @@ impl WindowManager {
                 e.value_mask.try_into().unwrap(),
                 &mut changes,
             );
+
+            trace!("Configured window {}", e.window);
         }
     }
 
-    fn on_configure_notify(&mut self, e: XConfigureEvent) {}
+    fn on_configure_notify(&mut self, _e: XConfigureEvent) {}
 
-    fn on_create_notify(&mut self, e: XCreateWindowEvent) {}
+    fn on_create_notify(&mut self, e: XCreateWindowEvent) {
+        trace!("Window {} created", e.window);
+    }
 
-    fn on_destroy_notify(&mut self, e: XDestroyWindowEvent) {}
+    fn on_destroy_notify(&mut self, e: XDestroyWindowEvent) {
+        trace!("Window {} destroyed", e.window);
+    }
 
-    fn on_reparent_notify(&mut self, e: XReparentEvent) {}
+    fn on_reparent_notify(&mut self, e: XReparentEvent) {
+        trace!("Window {} reparented", e.window);
+    }
 
-    extern "C" fn on_x_error(display: *mut Display, e: *mut XErrorEvent) -> i32 {
+    extern "C" fn on_x_error(_: *mut Display, e: *mut XErrorEvent) -> i32 {
         let e = unsafe { &*e };
-        eprintln!("X Error: {:?}", e);
+        error!("X Error: {:?}", e);
 
         0
     }
-    extern "C" fn on_wm_detected(display: *mut Display, e: *mut XErrorEvent) -> i32 {
+    extern "C" fn on_wm_detected(_: *mut Display, e: *mut XErrorEvent) -> i32 {
         assert_eq!(unsafe { (&*e).error_code }, BadAccess);
 
         WM_DETECTED.store(true, Ordering::Relaxed);
